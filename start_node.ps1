6214368a0ab4d9b437fde0a84e6b291934a42116
Viktor Reinhold <viktor.reinhold@gmail.com>
2018-08-09 12:32:24 +0200

    added node start script.

---
+++ start_node.ps1
@@ -0,0 +1,17 @@
+[CmdletBinding()]
+Param (
+    [int] $PortOffset = 1,
+
+    [Switch] $ConnectToNode1
+)
+
+$env:HTTP_PORT = (3000 + $PortOffset)
+$env:P2P_PORT = (6000 + $PortOffset)
+
+Remove-Item Env:\PEERS -ErrorAction SilentlyContinue
+
+if ($ConnectToNode1) {
+    $env:PEERS = "ws://localhost:6001"
+}
+
+npm start
